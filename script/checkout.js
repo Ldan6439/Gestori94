3600985912f10a22d40e0f00e82478a2d7573afd
Ldan6439 <lidannelk@gmail.com>
2025-08-13 12:51:23 -0400

    Delete script directory

--- script/checkout.js
+++
@@ -1,9 +0,0 @@
-const toggleAddressField = () => {
-  const addressField = document.getElementById('customerAddress');
-  const deliveryOption = document.getElementById('deliveryOption').value;
-  if (deliveryOption === 'mensajeria') {
-    addressField.classList.remove('hidden');
-  } else {
-    addressField.classList.add('hidden');
-  }
-};
